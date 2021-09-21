08f71890f8245245e4365f772795a0c81a9db50f
A Alver <wodafone@hotmail.com>
2021-09-21 05:43:09 +0100

    more commits

--- src/test/java/GitProject/ebay_Project.java
+++ src/test/java/GitProject/ebay_Project.java
@@ -1,14 +1,14 @@
 package GitProject;
 
-public class ebay_Proje {
+public class ebay_Project {
 
     public static void main(String[] args) {
         System.out.println("Hello");
-
+        System.out.println("more commit");
         System.out.println("Hello everyone");
 
     }
 
 
     }
-}
+
